17b14723e6952de7ddcf0b52ebc66cddd7838ef2
Grant Sweeney <grantsweeney02@gmail.com>
2022-07-23 19:48:55 -0400

    starting quiz app

---
+++ 2. quiz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: "When is my birthday?",
+        a: 'Oct 4',
+        b: 'Oct 8',
+        c: 'Oct 16',
+        d: 'Oct 18',
+        correct: 'b'
+    },
+    {
+        question: "What was my first programming language?",
+        a: 'Python',
+        b: 'C',
+        c: 'JavaScript',
+        d: 'Java',
+        correct: 'd' 
+    },
+    {
+        question: "What is my dog's name?",
+        a: 'London',
+        b: 'Berlin',
+        c: 'Las Vegas',
+        d: 'Tokyo',
+        correct: 'a'
+    },
+    {
+        question: "Who was my roommate first-year at college?",
+        a: 'Reid',
+        b: 'Oscar',
+        c: 'David',
+        d: 'Didn\'t have one!',
+        correct: 'a'
+    },
+    {
+        question: "Where was I born?",
+        a: 'United States',
+        b: 'Germany',
+        c: 'England',
+        d: 'Mars - I\'m an alien',
+        correct: 'a'
+    }
+]
